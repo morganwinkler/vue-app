7bd5d7b4d7cea67fbf61c6006619d41c6a063526
Morgan Winkler <103147883+morganwinkler@users.noreply.github.com>
2023-12-11 13:29:21 -0500

    Make add employee form work

--- src/components/EmployeeForm.vue
+++ src/components/EmployeeForm.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="employee-form">
-    <form>
+    <!-- @submit shorthand for v-on:submit, prevent is same as event.preventDefault() -->
+    <form @submit.prevent="handleSubmit">
       <label>Employee name</label>
       <!-- v-model is built in for updating an input value with an onchange event -->
       <input v-model="employee.name" type="text" />
@@ -21,6 +22,15 @@ export default {
         email: "",
       },
     };
+  },
+  // the methods object will contain all the custom methods created
+  methods: {
+    handleSubmit() {
+      console.log("testing handleSubmit");
+      // this.$emit('name-of-emitted-event', dataToPass)
+      // this is how we pass data back up to App
+      this.$emit("add:employee", this.employee);
+    },
   },
 };
 </script>
